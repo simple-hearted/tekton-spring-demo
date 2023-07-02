d59e164ad8d62ef167ecc8f6c189b1c49523bbd4
lanny <747180384@qq.com>
2023-07-02 22:16:34 +0800

    spring-demo init

---
+++ src/main/java/cn/simple/tektonspringdemo/TektonSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.simple.tektonspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TektonSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TektonSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/simple/tektonspringdemo/TektonSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.simple.tektonspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
